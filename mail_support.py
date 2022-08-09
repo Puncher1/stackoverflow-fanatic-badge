6dbbf037a3f5123eaed73aa3ef01a5caaf2eec84
Puncher <65789180+Puncher1@users.noreply.github.com>
2022-08-09 13:36:41 +0200

    Human Verification check and email notification

--- mail_support.py
+++ mail_support.py
@@ -4,7 +4,9 @@ import sys
 
 import smtplib
 from email.message import EmailMessage
-from email.utils import formataddr
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class __EmailClient:
 
@@ -29,26 +31,34 @@ def __get_traceback_msg(exc_type, exc_error, exc_tb):
 
 def send_error(subject_error):
     exc_error = sys.exc_info()[1]
-    exc_type = type(exc_error)
-    exc_tb = exc_error.__traceback__
+    if exc_error is None:
+        html_content = f"""
+                        <h4>Human Verification after login.</h4>
+                        <p>Action required!</p>
 
-    traceback_msg = __get_traceback_msg(exc_type, exc_error, exc_tb)
-    print(traceback_msg)
+                        <h6>This is an automated message.</h6>
+                        """
+    else:
+        exc_type = type(exc_error)
+        exc_tb = exc_error.__traceback__
+
+        traceback_msg = __get_traceback_msg(exc_type, exc_error, exc_tb)
+        print(traceback_msg)
+
+        html_content = f"""
+                <h4>An unexpected error has occurred!</h4>
+
+                <code>
+                    {traceback_msg}
+                </code>    
+
+                <h6>This is an automated message.</h6>
+                """
 
     msg = EmailMessage()
     msg["Subject"] = f"[ERR] - {subject_error}"
     msg["From"] = __client.email_from
     msg["To"] = __client.email_to
-
-    html_content = f"""
-        <h4>An unexpected error has occurred!</h4>
-        
-        <code>
-            {traceback_msg}
-        </code>    
-        
-        <h6>This is an automated message.</h6>
-        """
     msg.add_alternative(html_content, subtype="html")
 
     with smtplib.SMTP(__client.smtp_address) as server:
@@ -57,16 +67,17 @@ def send_error(subject_error):
         server.send_message(msg)
 
 
-def send_passed():
+def send_passed(login_streak):
     msg = EmailMessage()
     msg["Subject"] = f"[INFO] - Logged into stackoverflow.com"
     msg["From"] = __client.email_from
     msg["To"] = __client.email_to
-
+    print(__client.email, __client.email_from, __client.email_to, __client.smtp_address)
     print("Logged into stackoverflow.com and accessed profile page.")
 
     html_content = f"""
             <h4>Successfully logged into stackoverflow.com and accessed profile page!</h4>
+            <p>You have now {login_streak} consecutive logins!   
             
             <h6>This is an automated message.</h6>
             """
--- stackoverflow.py
+++ stackoverflow.py
@@ -1,6 +1,8 @@
 import os
 import time
+import re
 
+import selenium.common
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
@@ -28,6 +30,17 @@ def login():
         driver.find_element(By.ID, "submit-button").submit()
         time.sleep(1)
 
+        # Human Verification check
+        try:
+            driver.find_element(By.XPATH, '//*[@id="content"]/div/div/div[2]/h1')
+        except selenium.common.NoSuchElementException:
+            pass
+        else:
+            mail_support.send_error("Human Verification!")
+            return
+
+        time.sleep(1)
+
         # profile button
         driver.find_element(By.XPATH, "/html/body/header/div/ol[2]/li[2]/a").click()
         time.sleep(1)
@@ -37,7 +50,12 @@ def login():
             expected_conditions.presence_of_element_located((By.XPATH, '//*[@id="mainbar-full"]/div[1]/div[1]/div/div/div[1]'))
         )
         assert display_name in elem.text
-        mail_support.send_passed()
+        time.sleep(1)
+
+        visit_text = driver.find_element(By.XPATH, '//*[@id="js-daily-access-calendar-container"]/button/div[2]').text
+        login_streak = re.findall("\d+", visit_text)[1]
+
+        mail_support.send_passed(login_streak)
 
     except Exception as e:
         mail_support.send_error(e.__class__.__name__)
